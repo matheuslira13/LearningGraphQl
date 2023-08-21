91c653698cf475589be9615289d47a26f866d380
matheuslira13 <matheus_lira13@hotmail.com>
2023-08-21 16:08:31 -0300

    feat: Adiciona typescript com sucrase

--- src/config/index.js
+++
@@ -1,2 +0,0 @@
-// Nao criei nada aqui a ideia dentro de config errra criar uma pasta para dataSources contex e error , mas eu achei
-// melhor deixar tudo dentro de index do para uma melhor consulta e facilidade de uso
---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, name: "Matheus Lira Barbosa", email: "matheus@matheu" },
+  { id: 2, name: "Mozao", email: "mozao@matheu" },
+  { id: 3, name: "Vitamina", email: "vitamina@matheu" },
+];
